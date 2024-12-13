64e437d7c0dbbf04220259afa43fcf227a9c6ee4
sujana26 <sujanasri2609@gmail.com>
2024-12-13 10:20:07 +0530

    push

--- newapp/urls.py
+++ newapp/urls.py
@@ -14,7 +14,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Home page
+    path('', views.home, name='home'),# Home page
     path('upload/', views.upload_flower, name='upload_flower'),  # Upload flower page
     path('flowers/', views.flower_list, name='flower_list'),  # Flower list page
     path('result/', views.result_page, name='result_page'),
